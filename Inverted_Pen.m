7c390ab04c05a9a7952b96a72d17745bf33c3a3a
Solomon Chibuzo Nwafor <63534670+solonso@users.noreply.github.com>
2023-10-20 18:18:28 +0100

    Update Inverted_Pen.m

--- Inverted_Pen.m
+++ Inverted_Pen.m
@@ -1,16 +1,16 @@
-%% Init parameter
-g=9.81;         %gravity
-l=2.5;          %length
-m=2;            %mass
-Ts=0.01;        %sample time
-x_0= [pi/18 0]; %intial condtion
+%% Initial the pendulum parameters
+g=9.81;         %gravity of the pendulum mass (m/s^2)
+l=2.5;          %arm length of the pendulum (m)
+m=2;            %mass of the pendulum (kg)
+Ts=0.01;        %sample time (s)
+x_0= [pi/18 0]; %intial condtions for the pendulum angular position and angular velocity (rad, rad/s^2)
 
-%% State space
+%% State space formulation of the pendulum
 A= [0 1;(-g/l) 0];  %State matrix
 B= [0;1/(m*l^2)];   %input vector
 C= [1 0];           %Output matrix/vector
 D= 0;               %Disturbance matrix
 
-%% Kalman filter
+%% Kalman filter Covariance parameters for both the process and measurement noises
 Q=2e-4;             %Process noise covariance
 R=2e-4;             %Measurement noise covariance
